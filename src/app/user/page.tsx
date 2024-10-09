7f1d8b07737e37d939ca5cdd584e08311d383e4d
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-10 01:25:30 +0900

    feat: UserCard 구현

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,5 +1,12 @@
+import UserCard from "@/view/user/index/component/UserCard";
+
 const UserDashboardPage = () => {
-  return <div>UserDashboardPage</div>;
+  return (
+    <div>
+      UserDashboardPage
+      <UserCard />
+    </div>
+  );
 };
 
 export default UserDashboardPage;
